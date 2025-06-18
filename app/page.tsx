e6c13c5ba9587d113770149a254be1dbce3fd3af
Anastasiia Pashkevych <anastasiia.pashk@gmail.com>
2025-06-18 16:07:23 -0400

    create a navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-black bg-[#121212] container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-black bg-[#121212]">
+      <Navbar></Navbar>
+      <div className="container mx-auto px-12 py-4">
         <HeroSection />
-      </main>
+      </div>
+    </main>
   );
 }
