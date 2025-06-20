4bfdac726d45539cd5cb6856351ff87f14973797
Anastasiia Pashkevych <anastasiia.pashk@gmail.com>
2025-06-20 19:27:37 -0400

    add buttons for selection the project type

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -64,6 +64,10 @@ const ProjectsSection = () => {
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
+      <div className="text-white flex flex-row justify-center itens-center gap-2 py-6">
+        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">All</button>
+        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">Web</button>
+      </div>
       <div>
         {projectsData.map((project) => (
           <ProjectCard
